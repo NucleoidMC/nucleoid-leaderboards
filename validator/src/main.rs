334208eb630f8e63b9c50d10a54fb7026b0e1097
Ash (ashisbored) <ash.b.7558@gmail.com>
2022-01-04 19:57:41 +0000

    Add en_us translations for leaderboard names, and add translations check to validator

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1,17 +1,28 @@
-use std::fs::File;
+use std::{fs::File, collections::HashMap};
 use walkdir::WalkDir;
 use nucleoid_leaderboards::model::LeaderboardDefinition;
 
 fn main() {
+    let translations = load_translations();
     let mut failed = false;
 
     for entry in WalkDir::new("leaderboards/").into_iter()
         .filter_map(|e| e.ok()) {
         let filename = entry.file_name().to_string_lossy();
         if filename.ends_with(".json") {
-            if let Err(e) = serde_json::from_reader::<_, LeaderboardDefinition>(&File::open(entry.path()).unwrap()) {
-                println!("{:?}: {}", entry.path(), e);
-                failed = true;
+            match serde_json::from_reader::<_, LeaderboardDefinition>(&File::open(entry.path()).unwrap()) {
+                Ok(leaderboard) => {
+                    let (ident, path) = leaderboard.id.split_once(":").unwrap();
+                    let key = format!("leaderboard.{}.{}", ident, path);
+                    if let None = translations.get(&key) {
+                        eprintln!("Missing translation for leaderboard {} ({}) in en_us language!", leaderboard.id, filename);
+                        failed = true;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("{:?}: {}", entry.path(), e);
+                    failed = true;
+                }
             }
         }
     }
@@ -19,4 +30,8 @@ fn main() {
     if failed {
         std::process::exit(1);
     }
+}
+
+fn load_translations() -> HashMap<String, String> {
+    serde_json::from_reader(&File::open("translations/en_us.json").unwrap()).expect("Failed to parse en_us translations file")
 }
